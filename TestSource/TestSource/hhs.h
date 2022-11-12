87d6c1717216e43f925280743c2f388fb73ff20e
haha <824792205@qq.com>
2022-11-12 12:53:03 +0800

    hhs
    新建hhs

---
+++ TestSource/TestSource/hhs.h
@@ -0,0 +1,16 @@
+//
+//  hhs.h
+//  TestSource
+//
+//  Created by LiuHaH on 2022/11/12.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface hhs : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestSource/TestSource/hhs.m
@@ -0,0 +1,12 @@
+//
+//  hhs.m
+//  TestSource
+//
+//  Created by LiuHaH on 2022/11/12.
+//
+
+#import "hhs.h"
+
+@implementation hhs
+
+@end
