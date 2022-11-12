c8c8324f749fd2b93ca24017be56e68635165ce9
haha <824792205@qq.com>
2022-11-12 12:56:57 +0800

    kio
    新建kio

---
+++ TestSource/TestSource/kio.h
@@ -0,0 +1,16 @@
+//
+//  kio.h
+//  TestSource
+//
+//  Created by LiuHaH on 2022/11/12.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface kio : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestSource/TestSource/kio.m
@@ -0,0 +1,12 @@
+//
+//  kio.m
+//  TestSource
+//
+//  Created by LiuHaH on 2022/11/12.
+//
+
+#import "kio.h"
+
+@implementation kio
+
+@end
